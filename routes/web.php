a759f7f0d706064f5b29c9ee10afdc916bb8fdea
Jure Lokovsek <lokovsek.jure@gmail.com>
2022-11-23 23:39:16 +0100

    Removed UserProfile Component and connected code.

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,17 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 use App\Http\Livewire\Auth\ForgotPassword;
+use App\Http\Livewire\Auth\Login;
 use App\Http\Livewire\Auth\ResetPassword;
 use App\Http\Livewire\Auth\SignUp;
-use App\Http\Livewire\Auth\Login;
-use App\Http\Livewire\Dashboard;
 use App\Http\Livewire\Billing;
+use App\Http\Livewire\Dashboard;
+use App\Http\Livewire\LaravelExamples\UserManagement;
 use App\Http\Livewire\Profile;
-use App\Http\Livewire\Tables;
 use App\Http\Livewire\StaticSignIn;
 use App\Http\Livewire\StaticSignUp;
-
-use App\Http\Livewire\LaravelExamples\UserProfile;
-use App\Http\Livewire\LaravelExamples\UserManagement;
+use App\Http\Livewire\Tables;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,7 +24,7 @@ use App\Http\Livewire\LaravelExamples\UserManagement;
 |
 */
 
-Route::get('/', function() {
+Route::get('/', function () {
     return redirect('/login');
 });
 
@@ -36,7 +33,7 @@ Route::get('/login', Login::class)->name('login');
 
 Route::get('/login/forgot-password', ForgotPassword::class)->name('forgot-password');
 
-Route::get('/reset-password/{id}',ResetPassword::class)->name('reset-password')->middleware('signed');
+Route::get('/reset-password/{id}', ResetPassword::class)->name('reset-password')->middleware('signed');
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', Dashboard::class)->name('dashboard');
@@ -45,7 +42,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/tables', Tables::class)->name('tables');
     Route::get('/static-sign-in', StaticSignIn::class)->name('sign-in');
     Route::get('/static-sign-up', StaticSignUp::class)->name('static-sign-up');
-    Route::get('/laravel-user-profile', UserProfile::class)->name('user-profile');
     Route::get('/laravel-user-management', UserManagement::class)->name('user-management');
 });
 
